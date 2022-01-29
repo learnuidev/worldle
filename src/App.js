3131ca4031d7ef85668d3dbe80fbc8ab53772777
learnuidev <learnuidev@gmail.com>
2022-01-28 22:48:38 -0500

    MVP 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import { wordsDictionary } from './constants'
+
+// Steps:
+// 1. Define game state
+// 2. Define handlers
+
+
+const secret = 'BLISS'
 
 function App() {
+  // 1. game state
+  const [guessHistory, setGuessHistory] = useState([])
+  const [currentGuess, setCurrentGuess] = useState('')
+
+  // Define event handlers (3)
+
+  // 1. handler - handles characters
+  const handleChar = char => {
+    // guard
+    if (currentGuess.length < 5) {
+      setCurrentGuess(`${currentGuess}${char}`.toUpperCase())
+    }
+  }
+
+  // 2. handles deleting charater
+  const handleDelete = () => {
+    // handle delete
+    setCurrentGuess(currentGuess.slice(0, -1))
+  }
+
+  // 3. handles submittion
+  const handleEnter = () => {
+    // check if the currentGuess has 5 chars
+    if (currentGuess.length < 5) {
+      alert("Not enough letters")
+    } else if (secret === currentGuess) {
+      alert('You win')
+    } else if (!wordsDictionary.includes(currentGuess)) {
+      alert('Word not in list')
+    }
+    else if (guessHistory.length === 5) {
+      alert('You lose')
+    }
+    else {
+      // add the current guess to guessHistory
+      // reset currentGuess
+      setGuessHistory(guessHistory.concat(currentGuess))
+      setCurrentGuess('')
+    }
+  }
+
+  const handleKeyup = (event) => {
+    if (event.key === 'Backspace') {
+      handleDelete()
+    } else if (event.key === "Enter") {
+      handleEnter()
+    } else {
+      handleChar(event.key)
+    }
+  }
+
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup)
+    // cleanup function
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleChar, handleDelete, handleEnter])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" onKeyUp={handleKeyup}>
+      <header>
+        unwordle
       </header>
+      <main>
+        <div>
+          {guessHistory.map((guess, idx) => {
+            return (<div key={`${guess}-${idx}`}> {guess}</div>)
+          })}
+        </div>
+        <div>
+          {currentGuess}
+        </div>
+      </main>
     </div>
   );
 }
