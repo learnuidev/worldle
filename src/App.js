b66ec4f30dfbf9da250026358de06d7ef39732e6
learnuidev <learnuidev@gmail.com>
2022-01-30 21:59:17 -0500

    Added css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,93 @@ console.log("worldle", world)
 // Steps:
 // 1. Define game state
 // 2. Define handlers
+// 3. Custom Components - KeyList
+// 4. Custom Component - GameBoard
+
+
+// Custom Components
+function KeyList(props) {
+  return <div id="key-list">
+    {props.keyList.map(key => {
+      return (<button onClick={() => props.handleKeyBoard(key)}>{key}</button>)
+    })}
+  </div>
+}
+
+function GameTile(props) {
+  const { letter } = props
+  return (
+    <div class="game-tile">
+      {letter}
+    </div>
+  )
+}
+
+function GameRow(props) {
+  const { guess } = props;
+  return (
+    <div class="game-row">
+      {guess.map(letter => {
+        return <GameTile letter={letter} />
+      })}
+    </div>
+  );
+}
+
+function transformForView(currentGuess) {
+  const guessColl = currentGuess.split("")
+  return Array(5).fill("").map((i, idx) => {
+    if (guessColl[idx]) {
+      return guessColl[idx]
+    }
+    return " "
+  })
+}
+
+function GameBoard(props) {
+  const { guessHistory, currentGuess } = props
+  const guessHistoryLength = guessHistory.length
+  const empties = Array(5 - guessHistory.length).fill("     ")
+  console.log("EMPTIES", empties)
+  return (
+
+    <main class="game-board-container">
+      <div class="game-board">
+        {guessHistory.map((guess, idx) => {
+          return (<GameRow key={`${guess}-${idx}`} guess={guess.split("")} />)
+        })}
+        <GameRow guess={transformForView(currentGuess)} />
+
+        {/* <GameRow  */}
+        {empties.map((guess, idx) => {
+          return (<GameRow key={`${guess}-${idx}`} guess={guess.split("")} />)
+        })}
+      </div>
+    </main>
+
+  )
+}
+
+// FUTURE WORK:
+// Convert keys items from string to object
+// Why? - can metadata such as cutom key color,
+// User can set their own color
+function KeyBoard(props) {
+  const { on, handleKeyBoard } = props
+  const keys = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', "Backspace"]
+  ]
+
+  return (
+    <div id="keyboard">
+      {keys.map(keyList => {
+        return <KeyList keyList={keyList} handleKeyBoard={handleKeyBoard} />
+      })}
+    </div>
+  )
+}
 
 
 const secret = 'PARIS'
@@ -54,14 +141,18 @@ function App() {
     }
   }
 
-  const handleKeyup = (event) => {
-    if (event.key === 'Backspace') {
+  const handleKeyBoard = (value) => {
+    if (value === 'Backspace') {
       handleDelete()
-    } else if (event.key === "Enter") {
+    } else if (value === "Enter") {
       handleEnter()
     } else {
-      handleChar(event.key)
+      handleChar(value)
     }
+  }
+
+  const handleKeyup = (event) => {
+    handleKeyBoard(event.key)
   }
 
 
@@ -73,20 +164,12 @@ function App() {
 
 
   return (
-    <div className="App" onKeyUp={handleKeyup}>
+    <div className="game" onKeyUp={handleKeyup}>
       <header>
         worldle
       </header>
-      <main>
-        <div>
-          {guessHistory.map((guess, idx) => {
-            return (<div key={`${guess}-${idx}`}> {guess}</div>)
-          })}
-        </div>
-        <div>
-          {currentGuess}
-        </div>
-      </main>
+      <GameBoard guessHistory={guessHistory} currentGuess={currentGuess} />
+      <KeyBoard handleKeyBoard={handleKeyBoard} />
     </div>
   );
 }
